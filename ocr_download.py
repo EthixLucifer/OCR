5e8507f0e80d428444ec7e18025a6528ee56b03f
Ethix Lucifer <ethixlucifer@gmail.com>
2025-02-21 13:11:36 +0530

    this 2nd komit

--- ocr_download.py
+++ ocr_download.py
@@ -3,5 +3,5 @@ from paddleocr import PaddleOCR
 ocr = PaddleOCR(lang='en')  # Use 'multilingual' if needed
 print("OCR initialized successfully!")
 
-
+print("Downloading the model...")
 #this is a new update
