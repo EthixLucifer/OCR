603cd1ab41efcde948c042f6ccc8712093192d73
ethixlucifer <luciferethix@gmail.com>
2025-02-21 14:58:42 +0530

    commit made by ethixlucifer

--- ocr_download.py
+++ ocr_download.py
@@ -4,10 +4,3 @@ ocr = PaddleOCR(lang='en')  # Use 'multilingual' if needed
 print("OCR initialized successfully!")
 
 print("Downloading the model...")
-
-# just trying what to do?
-# second commit
-# third commit
-# fourth commit
-# fifth commit
-# sixth commit
